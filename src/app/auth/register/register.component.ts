a49593ea520d09a266df92447e18adb0d974f9cf
unknown <ranjm198@gmail.com>
2025-04-13 15:57:22 +0200

    Second  commit

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,20 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { DividerModule } from 'primeng/divider';
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
+import { CheckboxModule } from 'primeng/checkbox';
+import { CommonModule } from '@angular/common';
+import { RadioButton } from 'primeng/radiobutton';
 
 @Component({
   selector: 'app-register',
-  imports: [  CardModule,
-      InputTextModule,
-      PasswordModule,
-      ButtonModule,
-      DividerModule],
+  imports: [ CommonModule, InputTextModule, ButtonModule, CheckboxModule, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-
+  ingredient!: string;
+  username: string = '';
+  email: string = '';
+  password: string = '';
+  saveDetails: boolean = true;
+  newsletter: boolean = false;
 }
